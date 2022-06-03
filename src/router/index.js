6bbb43f33796734af8b6746942d780ded728f718
yangdeyi <yangdeyi1201@foxmail.com>
2022-06-03 18:13:09 +0800

    路由404配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import store from '../store/index.js'
+import {ElMessageBox} from 'element-plus'
 
 function getAllNames(routes) {
   // 递归获取路由列表所有 name 字段值存至一个数组
@@ -44,7 +45,7 @@ const routes = [
       {
         path: '/home/project',
         name:'project',
-        component: () => {return import('../views/project.vue')},
+        component: () => {return import('../views/project.vue')}
       },
       {
         path: '/home/interface',
@@ -59,9 +60,18 @@ const routes = [
       {
         path: '/home/env',
         name: 'env',
-        component: () => {return import('../views/env.vue')},
+        component: () => {return import('../views/env.vue')}
       }
     ]
+  },
+  {
+    path: '/404',
+    component: () => {return import('../views/page404.vue')}
+  },
+  {
+    // 在路由数组最后一项: 正则匹配任意路径并重定向至 /404 路由
+    path: '/:pathMatch(.*)*',
+    redirect: '/404',
   }
 ]
 
@@ -83,11 +93,22 @@ router.beforeEach((to, from, next) => {
     return next()
   }
   else if (names.includes(to.name) && !token) {
-  //  访问非登录页+无token:直接跳转至登录页
-    return next({name: 'login'})
+  //  访问非登录页+无token:提示并跳转至登录页
+    ElMessageBox.alert(
+        '会话已经失效, 请重新登录',
+        '提示',
+        {confirmButtonText: '确定',
+          type: 'warning',
+          callback: () => {next({name: 'login'})}
+        }
+    ).then(() => {})
   }
   else if (names.includes(to.name) && token) {
     // 访问非登录路由页+有token:可直接访问到非登录路由页
+    return next()
+  }
+  else if (to.path === '/404') { // 通过路由路径判断
+    // 路由 404 配置
     return next()
   }
 })
---
+++ src/views/page404.vue
@@ -0,0 +1,14 @@
+<template>
+    <img src="../assets/images/404.png" alt="page404">
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  img {
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
